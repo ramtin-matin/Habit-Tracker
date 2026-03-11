d55c0bb7685ef4f6594f203053c575d3c82f2bfe
Ramtin Matin <ramtinmat@gmail.com>
2026-03-11 13:11:42 -0400

    fix cors regex and headers

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,13 @@ from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi import _rate_limit_exceeded_handler
 
-from config import CORS_ALLOWED_ORIGINS, CORS_ALLOW_HEADERS, CORS_ALLOW_METHODS
+from config import (
+    CORS_ALLOWED_ORIGINS,
+    CORS_ALLOW_ORIGIN_REGEX,
+    CORS_ALLOW_METHODS,
+    CORS_ALLOW_HEADERS
+)
+
 from routers.clusters import router as clusters_router
 from routers.habit_logs import router as habit_logs_router
 from routers.habits import router as habits_router
@@ -15,12 +21,14 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ALLOWED_ORIGINS,
+    allow_origin_regex=CORS_ALLOW_ORIGIN_REGEX,
     allow_credentials=False,
-    allow_methods=CORS_ALLOW_METHODS,
-    allow_headers=CORS_ALLOW_HEADERS,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(habits_router)
