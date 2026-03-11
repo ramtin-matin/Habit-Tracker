b33943a39e85f47efad8366c31af4621837c78f8
Ramtin Matin <ramtinmat@gmail.com>
2026-03-11 11:30:19 -0400

    config fix

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,8 @@
 from sqlmodel import Session, create_engine
 
-from config import DATABASE_URL, SQL_ECHO
+from backend.config import MYSQL_URL, SQL_ECHO
 
-engine = create_engine(DATABASE_URL, echo=SQL_ECHO, pool_pre_ping=True)
+engine = create_engine(MYSQL_URL, echo=SQL_ECHO, pool_pre_ping=True)
 
 def get_session():
     with Session(engine) as session:
